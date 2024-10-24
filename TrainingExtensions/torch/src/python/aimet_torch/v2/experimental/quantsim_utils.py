b5c010a2958ca4bce3f8068c3749380b998f2a97
Joel Polizzi <quic_jpolizzi@quicinc.com>
2024-10-24 15:59:08 -0700

    update to conditional
    Signed-off-by: Joel Polizzi <quic_jpolizzi@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/experimental/quantsim_utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/experimental/quantsim_utils.py
@@ -234,7 +234,11 @@ def set_matmul_second_input_producer_to_8bit_symmetric(sim: 'QuantizationSimMode
         return connected_graph._module_to_op_dict[original_module]
 
     def get_closest_producer(op: Op):
-        quant_module = quant_modules.get(op.dotted_name.removeprefix(f'{model_name}.'), None)
+        if quant_modules.get(op.dotted_name.startswith(f'{model_name}.')):
+            quant_module = quant_modules.get(op.dotted_name[len(f'{model_name}.'):])
+        else:
+            quant_module = quant_modules.get(op.dotted_name)
+        #quant_module = quant_modules.get(op.dotted_name.removeprefix(f'{model_name}.'), None)
         if quant_module:
             if quant_module.output_quantizers[0]:
                 return quant_module
